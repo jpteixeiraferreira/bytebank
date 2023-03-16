7229d9766f18d4ca2a6e236f78e0286d396a9e75
JP <jpteixeiraferreira@gmail.com>
2023-03-16 09:18:10 -0300

    Testando conceitos de encapsulamento em Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -12,21 +12,34 @@ void main() =>
       ),
     ));
 
-class Lista_transferencia extends StatelessWidget{
+class Lista_transferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[
-        Card(
-          child: ListTile(leading: Icon(Icons.monetization_on), title: Text('R\$ 4000.00')),
-        ),Card(
-          child: ListTile(leading: Icon(Icons.monetization_on), title: Text('R\$ 2000.00')),
-        ),Card(
-          child: ListTile(leading: Icon(Icons.monetization_on), title: Text('R\$ 1500.00')),
-        ),Card(
-          child: ListTile(leading: Icon(Icons.monetization_on), title: Text('R\$ 500.00')),
-        ),
-      ],
+        children: <Widget>[
+        Item_transferencia(quantidade: 500.0, conta: 001),
+        Item_transferencia(quantidade: 600.0, conta: 002),
+        Item_transferencia(quantidade: 700.0, conta: 001),
+    ]
+    ,
     );
   }
 }
+
+class Item_transferencia extends StatelessWidget {
+  double quantidade;
+  int conta;
+
+  Item_transferencia({ required this.quantidade, required this.conta});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: ListTile(
+            leading: Icon(Icons.monetization_on),
+            title: Text(quantidade.toString()),
+            subtitle: Text(conta.toString()),
+        ));
+  }
+}
+

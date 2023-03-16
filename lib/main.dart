f2cf9060af977fda16ce50d1c799ee3471504a4d
JP <jpteixeiraferreira@gmail.com>
2023-03-16 13:00:05 -0300

    Testando conceitos de encapsulamento /2

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() =>
-    runApp(MaterialApp(
-      home: Scaffold(
-        body: Lista_transferencia(),
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: () {},
-            child: Icon(Icons.add)),
-      ),
-    ));
+    runApp(Bytebank_app());
+class Bytebank_app extends StatelessWidget {
+  const Bytebank_app({Key? key}) : super(key: key);
 
-class Lista_transferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-        children: <Widget>[
-        Item_transferencia(quantidade: 500.0, conta: 001),
-        Item_transferencia(quantidade: 600.0, conta: 002),
-        Item_transferencia(quantidade: 700.0, conta: 001),
-    ]
-    ,
+    return
+      MaterialApp(
+        home: Scaffold(
+          body: Formulario_transferencia(),
+        ),
+      );
+  }
+}
+class Formulario_transferencia extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Criando Transferência')),
+      body: Column(
+        children: [
+          TextField(),
+          TextField(),
+
+        ],
+      )
     );
   }
 }
-
+class Lista_transferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
+      ),
+      body: Column(
+          children: <Widget>[
+          Item_transferencia(quantidade: 500.0, conta: 001),
+          Item_transferencia(quantidade: 600.0, conta: 002),
+          Item_transferencia(quantidade: 700.0, conta: 001),
+      ]
+      ,
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {},
+          child: Icon(Icons.add)),
+    );
+  }
+}
 class Item_transferencia extends StatelessWidget {
   double quantidade;
   int conta;
@@ -37,8 +61,8 @@ class Item_transferencia extends StatelessWidget {
     return Card(
         child: ListTile(
             leading: Icon(Icons.monetization_on),
-            title: Text(quantidade.toString()),
-            subtitle: Text(conta.toString()),
+            title: Text('Valor: R\$ ${quantidade.toStringAsFixed(2)}'),
+            subtitle: Text('Conta: ${conta.toString()}'),
         ));
   }
 }
